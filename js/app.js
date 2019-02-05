021b31b44b21fdee0a97379e218285035c9f6706
wisestMe <wyhzest@gmail.com>
2019-02-05 07:54:48 +0100

    initial commit

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ prompt("Do you love Jesus?");*/
 
 let yourNumber = prompt("choose a number 1-10");
 let randomNumber = Math.round(Math.random());
-alert(randomNumber);
-alert(yourNumber);
+alert("the lucky number is " + randomNumber);
+alert("your chosen number is " + yourNumber);
 
 if(yourNumber == randomNumber) {
 	alert("hurrayyy!!!");
