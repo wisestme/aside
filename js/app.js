44cecc3766b7518154977da6f8e1e7d7264e581f
wisestMe <wyhzest@gmail.com>
2019-02-25 07:44:41 +0100

    adds restriction to page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 confirm("I love Jesus");
 prompt("Do you love Jesus?");*/
 
+/*
 let yourNumber = prompt("choose a number 1-10");
 let randomNumber = Math.round(Math.random());
 alert("the lucky number is " + randomNumber);
@@ -11,4 +12,13 @@ if(yourNumber == randomNumber) {
 	alert("hurrayyy!!!");
 } else {
 	alert("try again");
+}
+*/
+let age = 25;
+let name = "Wisest";
+
+if(age > 14) {
+	alert("sorry this page is for kids");
+} else {
+	alert("Welcome " + name + "!")
 }
